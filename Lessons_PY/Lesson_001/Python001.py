d68dc7601196f4961a87fb80dd3cf2c3623a30c5
Iogan <ivan_gulyaev@yahoo.com>
2022-12-16 23:03:27 +0700

    Тренировочный файл по питону

---
+++ Lessons_PY/Lesson_001/Python001.py
@@ -0,0 +1,64 @@
+# # # # # # x = input('Enter your name:')
+# # # # # # print ("Hello", x)
+# # # # # a = int(input('Enter number:'))
+# # # # # print(a * 2)
+# # # # # a = int(input('Enter hours:'))
+# # # # # b = int(input('Enter minutes:'))
+# # # # # print(a*60 + b)
+# # # # # x = int(input())
+# # # # # h = int(input())
+# # # # # m = int(input())
+# # # # # sum = x+h*60+m
+# # # # # print(sum//60)
+# # # # # print (sum%60)
+# # # # # a = int(input())
+# # # # # print(a>=10 and a<100)
+# # # # a = True
+# # # # b = False
+# # # # print (a and b or not a and not b)
+# # # # x = int(input('Введите первоечисло:'))
+# # # # y = int(input('Введите второе число:'))
+# # # # if y !=0:
+# # # #     print(x/y)
+# # # # else:
+# # # #     print("Деление не возможно")
+# # # #     y = int(input('Введите чиcло не равное нулю:'))
+# # # #     if y ==0:
+# # # #         print("Вы обосрались")
+# # # #     else:
+# # # #         print(x/y)
+# # # A = int(input('Минимальное время сна в часах:'))
+# # # B = int(input('Максимальное время сна в часах:'))
+# # # H = int(input('Сколько спите в данный момент в часах:'))
+# # # if H < A:
+# # #     print('Недосып')
+# # # elif H > B:
+# # #     print('Пересып')
+# # # elif H >= A or H <=B: 
+# # #     print('Это нормально')
+# # # i = int(input())
+# # # if (i % 4 == 0 and i % 100 != 0) or i % 400 == 0:
+# # #     print("Високосный")
+# # # else:
+# # #     print('Обычный')
+# # print('aboba\n\n\naboba')
+# ##работа со строками##
+# # a = 'lol balabol'
+# # b = 'kek cheburek'
+# # print(a,"\n" +b) 
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# p = (a+b+c)/2
+# S =(p*(p-a)*(p-b)*(p-c))**0.5
+# print(S)
+# x = int(input())
+# A = -15<x<=12 or 14<x<17 or x >=19
+# print(A)
+x = float(input())
+y = float(input())
+s = input()
+if s in ('*'):
+    print(x*y)
+elif s in ('+'):
+    print(x+y)
